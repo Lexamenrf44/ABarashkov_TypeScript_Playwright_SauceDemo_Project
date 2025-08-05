7edf7acaf7855047baca3b213965aec6c6d57191
lexamenrf44 <alexander.stcstudent@gmail.com>
2025-08-05 17:44:10 +0200

    Removed debugging feature

--- src/specs/Filter.tests.spec.ts
+++ src/specs/Filter.tests.spec.ts
@@ -3,10 +3,9 @@ import { filterOptions } from '../data/filterOptions';
 
 Object.entries(filterOptions).forEach(([key, val]) => {
     test.describe(`Filter option by ${key} key test`, () => {
-        test(`Should filter inventory list by the "${val.filterOption}" filter option`, async ({ page, cookieLogin, inventoryPage }) => {
+        test(`Should filter inventory list by the "${val.filterOption}" filter option`, async ({ cookieLogin, inventoryPage }) => {
             await inventoryPage.waitUntilInventoryPageLoaded();
             await inventoryPage.filterByOption(val.filterOption);
-            await page.waitForTimeout(3000); // for visual debugging
             await inventoryPage.assertByOption(val);
         });
     });
