f85e4c324acf308a210e19ede912b51a0ac6b02a
lexamenrf44 <alexander.stcstudent@gmail.com>
2025-08-05 17:43:38 +0200

    Minor test.describe naming fix

--- src/specs/Filter.tests.spec.ts
+++ src/specs/Filter.tests.spec.ts
@@ -2,7 +2,7 @@ import { test } from '../fixtures/CookieLogin';
 import { filterOptions } from '../data/filterOptions';
 
 Object.entries(filterOptions).forEach(([key, val]) => {
-    test.describe(`Filter option: ${key}`, () => {
+    test.describe(`Filter option by ${key} key test`, () => {
         test(`Should filter inventory list by the "${val.filterOption}" filter option`, async ({ page, cookieLogin, inventoryPage }) => {
             await inventoryPage.waitUntilInventoryPageLoaded();
             await inventoryPage.filterByOption(val.filterOption);
